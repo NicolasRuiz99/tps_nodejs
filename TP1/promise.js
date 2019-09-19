5a69530ad9588b416098e6737ad7314664b922f4
NicolasRuiz99 <nicoruiz72@gmail.com>
2019-09-19 03:05:27 -0300

    act

---
+++ TP1/promise.js
@@ -0,0 +1,71 @@
+let empleados = [{
+    id: 1,
+    nombre: 'Tito'
+}, {
+    id: 2,
+    nombre: 'Pedro'
+}, {
+    id: 3,
+    nombre: 'Juan'
+}];
+
+let salarios = [{
+    id: 1,
+    salario: 3000
+}, {
+    id: 2,
+    salario: 4000
+}];
+
+let getEmpleado = (id) => {
+    return new Promise((resolve, reject) => {
+        // La promesa es una funcion, que tiene dos callback, resolve y reject
+        // El "resolve", se llama si la promesa es exitosa, si logra encontrar un empleado
+        // El "reject" se llama si no es exitosa, si no existe un empleado en la BBDD
+        let empleadoDB = empleados.find(empleado => empleado.id == id)
+
+        if (!empleadoDB) {
+            reject(`No existe el empleado con el ID ${ id }`)
+        } else {
+            resolve(empleadoDB)
+        }
+    })
+}
+
+let getSalario = (id) => {
+    return new Promise((resolve, reject) => {
+        let empleadoDB = empleados.find(empleado => empleado.id == id)
+
+        if (!empleadoDB) {
+            reject(`No existe el empleado con el ID ${ id }`)
+        } else {
+            let salarioDB = salarios.find(salario => salario.id == id)
+            if (!salarioDB) {
+                reject(`No existe un salario para ${ empleadoDB.nombre }`)
+            } else {
+                resolve({
+                    nombre: empleadoDB.nombre,
+                    salario: salarioDB.salario
+                })
+            }
+        }
+    })
+}
+
+/*
+getEmpleado(2)
+    .then(empleado => {
+        console.log(`El empleado con el ID ${empleado.id} es ${empleado.nombre}`);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+*/
+
+getSalario(3)
+    .then((res) => {
+        console.log(`El empleado ${res.nombre} tiene un salario de ${res.salario}`);
+    })
+    .catch(err => {
+        console.log(err);
+    })
